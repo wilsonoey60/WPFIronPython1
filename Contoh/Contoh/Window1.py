a151220839bd1c07e9376255f7d3bfa82ea07d27
Wilson Oey <oey_wilson@hotmail.com>
2020-12-15 04:30:10 +0700

    Add files via upload
    Silakan baca deskripsi dari https://kotakode.com/blogs/3334/Bagaimana-cara-membuat-aplikasi-GUI-menggunakan-IronPython-Part-2-(%22Hello-World!%22)  untuk proses pengerjaan proyek lebih lanjut

---
+++ Contoh/Contoh/Window1.py
@@ -0,0 +1,14 @@
+﻿import wpf
+
+from System.Windows import Application, Window
+
+class Window1(Window):
+    def __init__(self):
+        wpf.LoadComponent(self, 'Window1.xaml')
+        
+    def cetakhasil_Click(self, sender, e):
+        self.Hasil.Content = "Hello world!"
+        pass
+       
+if __name__ == '__main__':
+    Application().Run(MyWindow())
